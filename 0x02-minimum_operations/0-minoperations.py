4d17d3cde25250a2e46b74d40a62514df0ab800a
homemix <kenmutati@gmail.com>
2022-06-28 10:39:00 +0300

    Fix: 0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-:returns: the minimum number of operations to reach n characters starting with 1
+:returns: the minimum number of operations
+ to reach n characters starting with 1
 """
 
 
 def minOperations(n: int) -> int:
     """
-    Returns the minimum number of operations to reach n characters starting with 1
+    Returns the minimum number of operations
+     to reach n characters starting with 1
     """
     operations = 0
     min_operations = 2
